9e246a794cebeb0b34bd8e862bb0841d28751d78
Mohan Tanguturi <mohanreddy.tanguturi@gmail.com>
2014-03-22 16:33:18 -0700

    Updated Validator

---
+++ src/main/java/com/grievance/web/healthcare/bean/validators/AbstractVBValidator.java
@@ -0,0 +1,12 @@
+package com.grievance.web.healthcare.bean.validators;
+
+import org.springframework.validation.Errors;
+
+public abstract class AbstractVBValidator {
+	
+	protected void validateVB(Object object, Errors errors) {
+		
+	}
+
+	
+}
