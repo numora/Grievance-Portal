05ee6ae6f7dab97c867d91e89abf6bcfc1b429d2
unknown <yc05s2v@L4170150.federated.fds>
2014-04-02 17:28:15 -0700

    Committing the changes related to test data sql. Added two more insert queries.

--- src/main/resources/dbschemas/test-data.sql
+++ src/main/resources/dbschemas/test-data.sql
@@ -18,6 +18,8 @@ INSERT INTO USER
 VALUES 
 	(3, '1234', 'test3@test.com','2014-01-01 14:49:01' , 'test3','2014-01-01 14:49:01' , 'Y', 1, '2014-01-01 14:49:01', 'english',1);
 	
-	insert into provider(provider_id,provier_name,provider_speciality,provider_type) values (1,'sujan','sujan','sujan');
+	insert into provider(provider_id,provier_name,provider_speciality,provider_type) values (1,'Kaiser','Kaiser','Kaiser');
+	insert into provider(provider_id,provier_name,provider_speciality,provider_type) values (1,'Anthem','Anthem','Anthem');
+	insert into provider(provider_id,provier_name,provider_speciality,provider_type) values (1,'Anthem Kaiser','Anthem Kaiser','Anthem Kaiser');
 	insert into member(member_id,first_name,last_name,middle_initial,suffix,phone,address_id,provider_id) values (1,'sujan','kumar','v','v','4159909362',1,1)
 	
